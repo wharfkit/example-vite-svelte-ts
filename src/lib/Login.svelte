3abc0b2de6664c7d2ebd2a6527205a1fd3855181
Aaron Cox <aaron@greymass.com>
2023-07-31 14:27:50 -0700

    Updating to 1.0.0

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,55 +1,18 @@
 <script lang="ts">
     import { writable } from 'svelte/store'
     import { onMount } from 'svelte/internal'
-    import { Session, SessionKit } from '@wharfkit/session'
+    import { Chains, Session, SessionKit } from '@wharfkit/session'
     import { WebRenderer } from '@wharfkit/web-renderer'
     import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
-    import { WalletPluginCloudWallet } from '@wharfkit/wallet-plugin-cloudwallet'
 
     const ui = new WebRenderer()
     const session = writable<Session | null>(null)
 
     const sessionKit = new SessionKit({
         appName: 'demo',
-        chains: [
-            {
-                id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-                url: 'https://eos.greymass.com',
-                explorer: {
-                    prefix: 'https://bloks.io/transaction/',
-                    suffix: '',
-                },
-            },
-            {
-                id: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
-                url: 'https://telos.greymass.com',
-                explorer: {
-                    prefix: 'https://explorer.telos.net/transaction/',
-                    suffix: '',
-                },
-            },
-            {
-                id: '8fc6dce7942189f842170de953932b1f66693ad3788f766e777b6f9d22335c02',
-                url: 'https://api.uxnetwork.io',
-                explorer: {
-                    prefix: 'https://explorer.uxnetwork.io/tx/',
-                    suffix: '',
-                },
-            },
-            {
-                id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-                url: 'https://wax.greymass.com',
-                explorer: {
-                    prefix: 'https://waxblock.io/transaction/',
-                    suffix: '',
-                },
-            },
-        ],
+        chains: [Chains.Jungle4],
         ui,
-        walletPlugins: [
-            new WalletPluginAnchor(),
-            new WalletPluginCloudWallet(),
-        ],
+        walletPlugins: [new WalletPluginAnchor()],
     })
 
     onMount(async () => {
