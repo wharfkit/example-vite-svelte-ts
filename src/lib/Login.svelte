748dd2e1b4028353d03b3f7648d9e88922339023
Aaron Cox <aaron@greymass.com>
2023-09-13 15:58:35 -0700

    Changes to mirror includenull's code for debugging

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,26 +1,49 @@
 <script lang="ts">
     import { writable } from 'svelte/store'
     import { onMount } from 'svelte/internal'
-    import { Chains, Session, SessionKit } from '@wharfkit/session'
+    import {
+        Chains,
+        PlaceholderName,
+        Session,
+        SessionKit,
+    } from '@wharfkit/session'
+    import { Contract, ContractKit } from '@wharfkit/contract'
     import { WebRenderer } from '@wharfkit/web-renderer'
+    import { TransactPluginResourceProvider } from '@wharfkit/transact-plugin-resource-provider'
     import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
+    import { WalletPluginCloudWallet } from '@wharfkit/wallet-plugin-cloudwallet'
+    import { WalletPluginWombat } from '@wharfkit/wallet-plugin-wombat'
 
     const ui = new WebRenderer()
     const session = writable<Session | null>(null)
+    const account = writable<Account | null>(null)
+    const contract = writable<Contract | null>(null)
 
-    const sessionKit = new SessionKit({
-        appName: 'demo',
-        chains: [Chains.Jungle4],
-        ui,
-        walletPlugins: [new WalletPluginAnchor()],
-    })
+    const sessionKit = new SessionKit(
+        {
+            appName: 'demo',
+            chains: [Chains.WAX],
+            ui,
+            walletPlugins: [
+                new WalletPluginAnchor(),
+                new WalletPluginCloudWallet(),
+                new WalletPluginWombat(),
+            ],
+        },
+        {
+            transactPlugins: [new TransactPluginResourceProvider()],
+        }
+    )
 
     onMount(async () => {
         try {
             const restored = await sessionKit.restore()
-            console.log(restored)
             if (restored) {
                 session.set(restored)
+                const contractKit = new ContractKit({
+                    client: restored.client,
+                })
+                contract.set(await contractKit.load('eosio'))
             }
         } catch (e) {
             console.log('error caught in onMount', e)
@@ -37,6 +60,14 @@
         $session = null
     }
 
+    async function test() {
+        $session.transact({
+            action: $contract.action('claimgbmvote', {
+                owner: PlaceholderName,
+            }),
+        })
+    }
+
     async function transact() {
         const action = {
             account: 'eosio.token',
@@ -49,13 +80,15 @@
                 memo: 'Yay WharfKit! Thank you <3',
             },
         }
-        $session.transact({ action }, { broadcast: false }).catch((e) => {
-            console.log('error caught in transact', e)
-        })
+        $session.transact({ action }, { broadcast: false })
     }
 </script>
 
 {#if $session}
+    {#if $account}
+        <p>{$account.accountName}</p>
+    {/if}
+    <button class="primary" on:click={test}> claimgmbvote </button>
     <button class="primary" on:click={transact}>
         Test Transaction (No Broadcast)
     </button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { visualizer } from "rollup-plugin-visualizer";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [
+        svelte(),
+        nodePolyfills({
+            // Whether to polyfill `node:` protocol imports.
+            protocolImports: true,
+        }),
+        visualizer({
+            template: "treemap", // or sunburst
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: "analyse.html", // will be saved in project's root
+        }),
+    ],
+    optimizeDeps: {
+        esbuildOptions: {
+          // Node.js global to browser globalThis
+          define: {
+            global: "globalThis",
+          },
+        },
+    },
 })
